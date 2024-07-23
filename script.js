9c8c48d3c714b97c33d8743431ee64f18a98ca83
Dave Ring <davealring@gmail.com>
2024-07-23 09:00:34 -0400

    Rough loop framework for grid creation

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 function getSize() {
-    gridSize = prompt("Please enter grid size under 100: ")
+    let gridSize = prompt("Please enter grid size under 100: ");
     if (gridSize < 100) {
         return gridSize;
     } else {
         alert("Size is too large!");
     };
+};
+
+function createGrid(gridSize){
+    let gridArray = [];
+    for (i = 0; i<gridSize; i++) {
+        gridArray.push(document.createElement("div"));
+    };
+    gridArray.classList.add("pixel");
+    let containerDiv = document.getElementsByClassName(".container");
+    containerDiv.appendChild(gridArray);
+};
+
+function playGame(){
+    let gridSize = getSize();
+    createGrid(gridSize);
 }
+
+playGame();
